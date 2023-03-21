445f8f087e73e82ad2e48f2a7415a8f57d9b0794
iosonosempreio <iosonosempreio@users.noreply.github.com>
2023-03-21 14:55:45 +0000

    Deploying to gh-pages from @ IEEE-VIS-Arts-Program/2023@98e754be3c9c6cebd9223b81d2f291be12504e5d 🚀

---
+++ _next/static/Kt-U20h4_wnF0xnM3n_HP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/622-89bc6c7058ded97d.js","static/css/074dd1b8c5a08ba2.css","static/chunks/pages/index-4e047fad79be4d05.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
